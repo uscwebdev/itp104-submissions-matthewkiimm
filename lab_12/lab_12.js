2db473546e92e2ab8b6c57518a0040083e37a9cf
matthewkiimm <mkim5307@usc.edu>
2023-12-02 21:02:47 -0800

    final

--- lab_12/lab_12.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.img-gallery img').click(function() {
-        // Remove 'active-image' class from all images
-        $('.img-gallery img').removeClass('active-image');
-
-        // Add 'active-image' class to the clicked image
-        $(this).addClass('active-image');
-    });
-});
